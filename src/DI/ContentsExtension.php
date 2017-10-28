f53faf811088db8f1e8c0ff62d495f18378820c6
Jan Trejbal <jan.trejbal@gmail.com>
2017-10-28 13:17:15 +0200

    Update to PHP 7.1

--- src/DI/ContentsExtension.php
+++ src/DI/ContentsExtension.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jan
- * Date: 26. 10. 2014
- * Time: 17:38
- */
+declare(strict_types=1);
 
 namespace Trejjam\Contents\DI;
 
@@ -15,38 +10,40 @@ class ContentsExtension extends Nette\DI\CompilerExtension
 {
 	const TAG_CONTENTS_SUBTYPES = 'trejjam.contents.subtypes';
 
-	protected $defaults = [
-		'configurationDirectory' => '%appDir%/config/contents',
+	protected $default = [
+		'configurationDirectory' => 'config/contents',
 		'logDirectory'           => NULL,
 		'subTypes'               => [],
 	];
 
-	public function loadConfiguration()
+	public function loadConfiguration() : void
 	{
 		parent::loadConfiguration();
 
 		$builder = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
 
-		Nette\Utils\Validators::assert($config, 'array');
+		$this->default['configurationDirectory'] = $this->getContainerBuilder()->parameters['appDir'] . DIRECTORY_SEPARATOR . $this->default['configurationDirectory'];
+		$this->validateConfig($this->default);
+
+		Nette\Utils\Validators::assert($this->config, 'array');
 
 		$contentsArguments = [
-			$config['configurationDirectory'],
-			$config['logDirectory'],
+			$this->config['configurationDirectory'],
+			$this->config['logDirectory'],
 		];
-		if ( !is_null($config['logDirectory'])) {
+		if ( !is_null($this->config['logDirectory'])) {
 			$contentsArguments[2] = '@tracy.logger';
 		}
 
 		$builder->addDefinition($this->prefix('contents'))
-				->setClass('Trejjam\Contents\Contents')
+				->setType('Trejjam\Contents\Contents')
 				->setArguments($contentsArguments);
 
-		foreach ($config['subTypes'] as $subTypeName => $subType) {
+		foreach ($this->config['subTypes'] as $subTypeName => $subType) {
 			$def = $builder->addDefinition($this->prefix('contents.' . md5(Nette\Utils\Json::encode($subType))));
 			$def->addSetup('setName', [$subTypeName]);
 			$def->setFactory(
-				Nette\DI\Compiler::filterArguments(
+				Nette\DI\Helpers::filterArguments(
 					[
 						is_string($subType) ? new Nette\DI\Statement($subType) : $subType,
 					]
